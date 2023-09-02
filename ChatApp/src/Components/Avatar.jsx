3180bc41b11d06838890698b849979e804ab4bf1
ShahaanShahzad <mohammedshaheershahzadrawda5@gmail.com>
2023-09-02 10:59:48 +0500

    Ui in Process

--- ChatApp/src/Components/Avatar.jsx
+++ ChatApp/src/Components/Avatar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Avatar = ({ item }) => {
     return (
         <div className={`w-10 h-10 relative rounded-full flex items-center ${item.color}`}>
-            <div className='text-center text-xl w-full opacity-70'>{item.username[0]}</div>
+            <div className='text-center text-white text-xl w-full opacity-70'>{item.username[0]}</div>
             {
                 item.online && (
                     <div className='absolute w-3 h-3 bg-green-600 rounded-full bottom-0 right-0 border border-white shadow-lg'></div>
--- ChatApp/src/Components/Header.jsx
+++ ChatApp/src/Components/Header.jsx
@@ -1,11 +1,10 @@
-import React from 'react'
-import logo from "../assets/ChatApp.png"
+import logo from "/ChatApp.png"
 const Header = () => {
     return (
         <header className="bg-[#244254] h-[5rem] text-gray-600 body-font">
             <div className="container mx-auto flex flex-wrap py-2 flex-col md:flex-row items-center">
                 <a className=" cursor-pointer flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-<img src={logo} className='w-[4rem]  h-[4rem]' unselectable="on" alt="" />
+<img src={logo} className='w-[4rem]  h-[4rem]' alt="" />
                     <span className="logoText text-white ml-3 text-[1.6rem]">Chat App</span>
                     
                 </a>
